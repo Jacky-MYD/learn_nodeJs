d009f90221c059829d60ff01e8d1910a18dd79ed
Jacky <657414127@qq.com>
2018-03-27 14:02:37 +0800

    first commit

---
+++ Jacky/day03_http/http.js
@@ -0,0 +1,9 @@
+var http = require('http')
+
+http
+    .createServer(function (req, res) {
+        res.writeHrad(200, {'Content-Type': 'text/plain'})
+        res.write('Hellon Nodejs');
+        res.end
+    })
+    .listen(2018)
